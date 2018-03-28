ffa9b8b5518e3c6ed9a66eeac6ca880b466e347d
annexiao <56244595@qq.com>
2018-03-27 19:08:15 -0700

    331423

---
+++ session10.R
@@ -0,0 +1,19 @@
+library(stringr)
+
+
+
+print(c[",])
+print(\")
+cat("\"")
+cat("dso\n545")
+cat("dso\t545")
+cat(":-\\")
+cat("(^_^\")")
+cat("@_'-'")
+cat("\\m/")
+
+? str_locate
+? str_sub
+
+
+str_locate(string="great", pattern = "a")
